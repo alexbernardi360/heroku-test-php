46d585d5a2a82eab55e32d512c3c152c833d2e0f
Alessandro Bernardi <alexbernardi360@users.noreply.github.com>
2019-05-16 17:37:37 +0200

    Prima modifica di prova

--- index.php
+++ index.php
@@ -1,10 +1,1 @@
-<?php
-Header("Custom-Header: Ciao");
-Header("Content-Type: application/json");
-
-echo json_encode(array(
-    "property" => "value",
-    "other_property" => array(
-        "subprop" => 123
-    )
-));
+<?php echo "Ciao Mondo!" ?>
